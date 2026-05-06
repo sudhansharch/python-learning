28ea82a7a39bb88391900239c976118234b1a69a
sudhansharch <sudhanshuarchx@gmail.com>
2026-05-06 16:26:32 +0530

    Add files via upload

---
+++ sqrt.py
@@ -0,0 +1,6 @@
+# Take a number as input and print its square root using math module
+
+import math
+
+Yournumber = int(input("Enter your number for finding sqareroot : "))
+print(math.sqrt(Yournumber))
