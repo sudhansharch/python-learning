053c168825ae3139e8dbc616988c50706b3603c7
sudhansharch <sudhanshuarchx@gmail.com>
2026-05-07 15:40:21 +0530

    Add files via upload

---
+++ self_introduction.py
@@ -0,0 +1,20 @@
+# Simple self introduction program using Python input and variables
+
+name = input("Enter your full name: ")
+city = input("Enter your city with state: ")
+qualification = input("Enter your qualification: ")
+hobby = input("Enter your hobby: ")
+goal = input("Enter your goal: ")
+
+
+print("Good morning sir/madam.",
+"My name is",name,
+ "I am from",city,
+ "I completed my",qualification,
+"My hobbies are",hobby,
+"I am a hardworking and self-motivated person."
+"I always try to learn new things and improve my skills."
+
+"My goal is",goal,
+"That's all about me."
+"Thank you.")
