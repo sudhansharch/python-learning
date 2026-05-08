a7b75a137c19b669738a76a482d59f4a9e8a1232
sudhansharch <sudhanshuarchx@gmail.com>
2026-05-08 11:49:43 +0530

    Add files via upload

---
+++ basic_calculator.py
@@ -0,0 +1,10 @@
+# Making a basic calculator program using float instead of int
+
+num1 =(float(input("Enter your first number : ")))
+num2 =(float(input("Enter your second number :")))
+
+print("addition :", num1 + num2 )
+print("subtraction :", num1 - num2)
+print("multiplication :",num1 * num2)
+print("division :", num1 / num2)
+print("remainder :", num1 % num2)
