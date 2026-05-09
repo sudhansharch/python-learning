1ea03799f3415ea2eae6050df7d250e59b7ec5c6
sudhansharch <sudhanshuarchx@gmail.com>
2026-05-09 20:03:35 +0530

    Add files via upload

---
+++ student_result.py
@@ -0,0 +1,11 @@
+name = (input("student name : "))
+subject1 = int(input("python marks : "))
+subject2 = int(input("english marks : "))
+subject3 = int(input("math marks : "))
+
+total = subject1 + subject2 + subject3
+print(total)
+average = total / 3
+print(average)
+print("Your total marks is :", total, "and your average is ", average)
+
